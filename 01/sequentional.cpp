7b14cc92b241fcdd384381c17cbb272e541980ef
Petr Pondělík <pondepe1@fit.cvut.cz>
2021-02-21 22:20:40 +0100

    HW1: Working on loading the init state.

--- 01/sequentional.cpp
+++ 01/sequentional.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main(void) {
-    cout << "TEST" << endl;
+enum field {Rook, Knight, Pawn, Empty};
+
+class ChessBoard {
+
+private:
+    vector<int> board;
+
+public:
+    ChessBoard(void) {}
+    ChessBoard(int k, vector<string> & boardStr)
+    {
+        cout << "ChessBoard" << endl;
+        cout << k << endl;
+    };
+};
+
+class Game {
+
+private:
+    int maxDepth;
+    ChessBoard * chessBoard;
+//    moves
+//    active - Rook or Knight
+
+public:
+    Game(int maxDepth)
+    {
+        this->maxDepth = maxDepth;
+    }
+
+    void initGame(int k, vector<string> & boardStr)
+    {
+        cout << "Init game" << endl;
+        cout << this->maxDepth << endl;
+        this->chessBoard = new ChessBoard(k, boardStr);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    cout << "START" << endl;
+
+    int k, maxDepth;
+
+    cin >> k;
+    cout << k << endl;
+
+    cin >> maxDepth;
+    cout << maxDepth << endl;
+
+    string tmp;
+    vector<string> boardStr;
+    for (int i = 0; i < k; ++i) {
+        cin >> tmp;
+        boardStr.emplace_back(tmp);
+    }
+
+    Game * game = new Game(maxDepth);
+    game->initGame(k, boardStr);
 }
